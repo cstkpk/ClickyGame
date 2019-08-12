143c84686b78a86654ae2d8659ca44a4ad610b99
Cara Sunberg <cnsunberg@gmail.com>
2019-08-12 18:29:44 -0400

    Added basic code to App.js, also renamed a few files and folders

---
+++ app/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import DataCard from "./components/DataCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import data from "./data.json";
+
+class App extends Component {
+    // Setting this.state.data to the data json array
+    state = {
+      data
+    };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Clicky Game!</Title>
+        {this.state.data.map(data => (
+          <FriendCard
+            // removeFriend={this.removeFriend}
+            id={data.id}
+            key={data.id}
+            image={data.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+};
+
+export default App;
