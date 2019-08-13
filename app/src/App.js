1c9783d3b32c56f4d38f68a2dc3689162b1cef6d
Cara Sunberg <cnsunberg@gmail.com>
2019-08-12 20:15:10 -0400

    Working on shuffleData function and finding clicked state of data.id

--- app/src/App.js
+++ app/src/App.js
@@ -10,12 +10,13 @@ class App extends Component {
       data
     };
 
-    shuffleData = id => {
-        // Filter this.state.friends for friends with an id not equal to the id being removed
-        const friends = this.state.friends.filter(friend => friend.id !== id);
+    shuffleData = id => { // How to refer to the "clicked" property through id?
+        // For the data with the selected id, set "clicked" boolean to true
+        let data = this.state.data.clicked = true;
+        console.log("Shuffling!");
         // Set this.state.friends equal to the new friends array
-        this.setState({ friends });
-      };
+        this.setState({ data });
+    };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
@@ -37,3 +38,8 @@ class App extends Component {
 };
 
 export default App;
+
+// When user clicks an image, we want to trigger shuffleData(id);
+// shuffleData will check the clicked image:
+    // If the clicked image currently has a "clicked" boolean of false, it will change it to true and re-shuffle the images
+    // If the clicked image currently has a "clicked" boolean of true, it will end/restart the game
