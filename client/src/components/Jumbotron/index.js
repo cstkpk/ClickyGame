5c815ce2c7a3a691146e49424cb962d4ed06a735
Cara Sunberg <cnsunberg@gmail.com>
2019-09-28 21:24:42 -0400

    Edited text in jumbotron component

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,7 +4,7 @@ import { Jumbotron } from 'react-bootstrap';
 function Jumbo(props) {
     return(
         <Jumbotron className="bg-dark">
-            <h1 className="text-center text-light">Welcome to the Clicky Game!</h1>
+            <h1 className="text-center text-light">Remember the Vegetable!</h1>
             <p className="text-center text-light">Score: {props.score} | Wins: {props.wins} | Losses: {props.losses}</p>
         </Jumbotron>
     );
