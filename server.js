2837961bacdf6fa2ecc80f7d4d5f3f23d3a1aa86
Cara Sunberg <cnsunberg@gmail.com>
2019-09-28 20:30:23 -0400

    Added basic back end files and dependencies: *Basic server and routing files N.B. Everything works smoothly launching from client folder but the app crashes before it loads successfully when launching from server folder

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets for Heroku
+if (process.env.NODE_ENV === production) {
+    app.use(express.static("client/build"));
+}
+// Use morgan if in dev environment
+if (app.get("env") === "development") {
+    const logger = require("morgan");
+    app.use(logger("dev"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+// Start the API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
